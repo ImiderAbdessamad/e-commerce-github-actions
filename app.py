74812986856628b67462308b7e703e5d04df29c4
ImiderAbdessamad <a.imider4322@uca.ac.ma>
2024-11-25 18:29:07 +0100

    deploy database

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
+import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 # MySQL Configuration
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'ecommerce'
-
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
 mysql = MySQL(app)
 
 @app.route('/')
